9bc6be9006f6e62f8ef41e523b3cf314f7a5433b
Abdullah Nawaz <nawazabdullah470@gmail.com>
2023-08-08 15:00:12 +0500

    Add Azure SQL connection

--- src/main/Login.java
+++ src/main/Login.java
@@ -12,112 +12,101 @@ import java.sql.SQLException;
 @SuppressWarnings("serial")
 public class Login extends JFrame {
 
-    private JPanel contentPane;
-    private JLabel loginInstructionLabel;
+	private JPanel contentPane;
+	private JLabel loginInstructionLabel;
 
+	private JTextField usernameTF;
+	private JPasswordField passwordTF;
 
-    private JTextField usernameTF;
-    private JPasswordField passwordTF;
+	private JLabel logoImageLabel;
+	ImageIcon logoImage = new ImageIcon("SLF-Logo.png");
 
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
-    private JLabel logoImageLabel;
-    ImageIcon logoImage = new ImageIcon("SLF-Logo.png");
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 733, 453);
+		contentPane = new JPanel();
+		contentPane.setBackground(SystemColor.menu);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 
+		loginInstructionLabel = new JLabel("Login to SLF Database");
+		loginInstructionLabel.setFont(new Font("Heiti SC", Font.PLAIN, 25));
+		loginInstructionLabel.setBounds(413, 71, 320, 38);
+		contentPane.add(loginInstructionLabel);
 
-    /**
-     * Launch the application.
-     */
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    Login frame = new Login();
-                    frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
+		usernameTF = new JTextField();
+		usernameTF.setColumns(10);
+		usernameTF.setBounds(534, 146, 136, 26);
+		contentPane.add(usernameTF);
 
-    /**
-     * Create the frame.
-     */
-    public Login() {
-        setResizable(false);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 733, 453);
-        contentPane = new JPanel();
-        contentPane.setBackground(SystemColor.menu);
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        setContentPane(contentPane);
-        contentPane.setLayout(null);
+		JLabel usernameLabel = new JLabel("Username");
+		usernameLabel.setForeground(Color.BLACK);
+		usernameLabel.setFont(new Font("Heiti SC", Font.PLAIN, 13));
+		usernameLabel.setBackground(new Color(244, 255, 236));
+		usernameLabel.setBounds(427, 151, 91, 16);
+		contentPane.add(usernameLabel);
 
-        loginInstructionLabel = new JLabel("Login to SLF Database");
-        loginInstructionLabel.setFont(new Font("Heiti SC", Font.PLAIN, 25));
-        loginInstructionLabel.setBounds(413, 71, 320, 38);
-        contentPane.add(loginInstructionLabel);
+		JLabel passwordLabel = new JLabel("Password");
+		passwordLabel.setForeground(Color.BLACK);
+		passwordLabel.setFont(new Font("Heiti SC", Font.PLAIN, 13));
+		passwordLabel.setBounds(427, 220, 79, 16);
+		contentPane.add(passwordLabel);
 
-        usernameTF = new JTextField();
-        usernameTF.setColumns(10);
-        usernameTF.setBounds(534, 146, 136, 26);
-        contentPane.add(usernameTF);
+		JButton loginBTN = new JButton("LOGIN");
+		loginBTN.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent e) {
+		        String username = usernameTF.getText();
+		        String password = new String(passwordTF.getPassword());
 
-        JLabel usernameLabel = new JLabel("Username");
-        usernameLabel.setForeground(Color.BLACK);
-        usernameLabel.setFont(new Font("Heiti SC", Font.PLAIN, 13));
-        usernameLabel.setBackground(new Color(244, 255, 236));
-        usernameLabel.setBounds(427, 151, 91, 16);
-        contentPane.add(usernameLabel);
+		        Utility ut = new Utility(username, password);
 
-        JLabel passwordLabel = new JLabel("Password");
-        passwordLabel.setForeground(Color.BLACK);
-        passwordLabel.setFont(new Font("Heiti SC", Font.PLAIN, 13));
-        passwordLabel.setBounds(427, 220, 79, 16);
-        contentPane.add(passwordLabel);
+		        if (ut.conn != null) {
+		            MainMenu.main(null);
+		            dispose();
+		        } else {
+		            JOptionPane.showMessageDialog(null, "Username or Password is incorrect, TRY AGAIN");
+		        }
+		    }
+		});
+		loginBTN.setBounds(482, 279, 117, 29);
+		contentPane.add(loginBTN);
 
-        JButton loginBTN = new JButton("LOGIN");
-        loginBTN.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-//                try {
-//                    Utility ut = new Utility();
-//                    String sql = "SELECT USERNAME,USERPASSWORD FROM LOGIN WHERE USERNAME=? AND USERPASSWORD=?";
-//                    ut.pstmt = ut.conn.prepareStatement(sql);
-//                    ut.pstmt.setString(1, usernameTF.getText());
-//                    ut.pstmt.setString(2, passwordTF.getPassword().toString());
-//                    ut.rs = ut.pstmt.executeQuery();
-//                    if (ut.rs.next()) {
-                        MainMenu.main(null);
-                        dispose();
-//                    } else {
-//                        JOptionPane.showMessageDialog(null, "Username or Password is incorrect, please try again");
-//                        ut.conn.close();
-//                    }
-//
-//                } catch (SQLException e1) {
-//                    JOptionPane.showMessageDialog(null, "Username or Password is incorrect, TRY AGAIN");
-//                }
+		JPanel logoPane = new JPanel();
+		logoPane.setLayout(null);
+		logoPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		logoPane.setBackground(new Color(118, 180, 221));
+		logoPane.setBounds(0, 0, 375, 425);
+		contentPane.add(logoPane);
 
-            }
-        });
-        loginBTN.setBounds(482, 279, 117, 29);
-        contentPane.add(loginBTN);
+		logoImageLabel = new JLabel("");
+		logoImageLabel.setBounds(54, 123, 267, 164);
+		logoPane.add(logoImageLabel);
 
-        JPanel logoPane = new JPanel();
-        logoPane.setLayout(null);
-        logoPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        logoPane.setBackground(new Color(118, 180, 221));
-        logoPane.setBounds(0, 0, 375, 425);
-        contentPane.add(logoPane);
+		logoImageLabel.setIcon(logoImage);
 
-        logoImageLabel = new JLabel("");
-        logoImageLabel.setBounds(54, 123, 267, 164);
-        logoPane.add(logoImageLabel);
-
-        logoImageLabel.setIcon(logoImage);
-
-        passwordTF = new JPasswordField();
-        passwordTF.setBounds(534, 215, 136, 26);
-        contentPane.add(passwordTF);
-    }
+		passwordTF = new JPasswordField();
+		passwordTF.setBounds(534, 215, 136, 26);
+		contentPane.add(passwordTF);
+	}
 }
--- src/utility/Utility.java
+++ src/utility/Utility.java
@@ -1,26 +1,44 @@
 package utility;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Utility {
 
-    // Will replace with Properties
-    public String dburl = "";
-    public String user = "";
-    public String pass = "";
+    public String dburl = "jdbc:sqlserver://slf-db-server.database.windows.net:1433;database=SLFdb";
+    public String user;
+    public String pass;
     public Connection conn;
     public ResultSet rs;
     public Statement stmt;
     public PreparedStatement pstmt;
 
-    public Utility() {
+    public Utility(String user, String pass) {
+        this.user = user;
+        this.pass = pass;
+
         try {
-            conn = DriverManager.getConnection(dburl, user, pass);
+            // Load the SQL Server JDBC driver
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+            // Modify the JDBC URL to include the user and password
+            String completeDbUrl = dburl + ";user=" + user + ";password=" + pass
+                    + ";encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+
+            // Establish the connection using the modified JDBC URL
+            conn = DriverManager.getConnection(completeDbUrl);
+
             if (conn != null) {
-                System.out.println("Connected to the database !");
-            } else {
-                System.out.println("Failed to connect to Database!");
+                System.out.println("Connected to the database!");
             }
+
+        } catch (ClassNotFoundException e) {
+            System.err.println("JDBC Driver not found");
+            e.printStackTrace();
         } catch (SQLException e) {
             System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
         }
@@ -28,14 +46,17 @@ public class Utility {
 
     public void terminate() {
         try {
-            pstmt.close();
-            conn.close();
-
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
         } catch (SQLException e) {
             System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
         }
-
     }
 
-
+    // Add other methods for database operations as needed
 }
+
