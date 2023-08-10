b0d59f58311374a0471210cb9d9c3aef73080ee6
Abdullah Nawaz <nawazabdullah470@gmail.com>
2023-08-10 18:06:01 +0500

    Add buttons to View Frame

--- src/viewData/ViewDataFrame.java
+++ src/viewData/ViewDataFrame.java
@@ -19,7 +19,7 @@ public class ViewDataFrame extends JFrame {
         sidePanel.setPreferredSize(new Dimension(190, getHeight()));
         sidePanel.setBackground(new Color(118, 185, 255));
 
-        JButton button1 = new JButton("View Beneficiary Data");
+        JButton button1 = new JButton("Beneficiaries");
         button1.setBounds(6, 126, 178, 34);
         button1.addActionListener(new ActionListener() {
             @Override
@@ -30,7 +30,7 @@ public class ViewDataFrame extends JFrame {
         sidePanel.setLayout(null);
         sidePanel.add(button1);
 
-        JButton button2 = new JButton("View Community Data");
+        JButton button2 = new JButton("Communities");
         button2.setBounds(6, 162, 178, 34);
         button2.addActionListener(new ActionListener() {
             @Override
@@ -50,7 +50,7 @@ public class ViewDataFrame extends JFrame {
         getContentPane().setLayout(new BorderLayout());
         getContentPane().add(sidePanel, BorderLayout.WEST);
         
-        JButton button2_1 = new JButton(" View Record Data");
+        JButton button2_1 = new JButton("Beneficiary and Records");
         button2_1.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
         	}
@@ -58,22 +58,66 @@ public class ViewDataFrame extends JFrame {
         button2_1.setBounds(6, 199, 178, 34);
         sidePanel.add(button2_1);
         
-        JButton button2_1_1 = new JButton(" ");
-        button2_1_1.setBounds(6, 341, 178, 60);
-        sidePanel.add(button2_1_1);
-        
-        JButton button2_1_1_1 = new JButton(" ");
-        button2_1_1_1.setBounds(6, 413, 178, 60);
-        sidePanel.add(button2_1_1_1);
-        
-        JButton button2_1_1_1_1 = new JButton(" ");
-        button2_1_1_1_1.setBounds(6, 485, 178, 60);
-        sidePanel.add(button2_1_1_1_1);
-        
         JButton logoBTN = new JButton("Logo Button");
         logoBTN.setBounds(6, 6, 178, 108);
         logoBTN.setIcon(new ImageIcon("SLF-Logo.png"));
         sidePanel.add(logoBTN);
+        
+        JButton button2_1_1 = new JButton("Vaccination Record");
+        button2_1_1.setBounds(6, 232, 178, 34);
+        sidePanel.add(button2_1_1);
+        
+        JButton button2_1_1_1 = new JButton("Sales Record");
+        button2_1_1_1.setBounds(6, 267, 178, 34);
+        sidePanel.add(button2_1_1_1);
+        
+        JButton button2_1_1_1_1 = new JButton("Predation Record");
+        button2_1_1_1_1.setBounds(6, 303, 178, 34);
+        sidePanel.add(button2_1_1_1_1);
+        
+        JButton button2_1_1_1_1_1 = new JButton("Disease Record");
+        button2_1_1_1_1_1.setBounds(6, 338, 178, 34);
+        sidePanel.add(button2_1_1_1_1_1);
+        
+        JButton button2_1_1_1_1_1_1 = new JButton("Slaughter Record");
+        button2_1_1_1_1_1_1.setBounds(6, 372, 178, 34);
+        sidePanel.add(button2_1_1_1_1_1_1);
+        
+        JButton button1_1 = new JButton(" ");
+        button1_1.setBounds(6, 405, 178, 34);
+        sidePanel.add(button1_1);
+        
+        JButton button2_2 = new JButton(" ");
+        button2_2.setBounds(6, 441, 178, 34);
+        sidePanel.add(button2_2);
+        
+        JButton button2_1_2 = new JButton(" ");
+        button2_1_2.setBounds(6, 478, 178, 34);
+        sidePanel.add(button2_1_2);
+        
+        JButton button2_1_1_2 = new JButton(" ");
+        button2_1_1_2.setBounds(6, 511, 178, 34);
+        sidePanel.add(button2_1_1_2);
+        
+        JButton button2_1_1_1_2 = new JButton(" ");
+        button2_1_1_1_2.setBounds(6, 546, 178, 34);
+        sidePanel.add(button2_1_1_1_2);
+        
+        JButton button2_1_1_1_1_2 = new JButton(" ");
+        button2_1_1_1_1_2.setBounds(6, 582, 178, 34);
+        sidePanel.add(button2_1_1_1_1_2);
+        
+        JButton button2_1_1_1_1_1_2 = new JButton(" ");
+        button2_1_1_1_1_1_2.setBounds(6, 617, 178, 34);
+        sidePanel.add(button2_1_1_1_1_1_2);
+        
+        JButton button2_1_1_1_1_1_1_1 = new JButton(" ");
+        button2_1_1_1_1_1_1_1.setBounds(6, 651, 178, 34);
+        sidePanel.add(button2_1_1_1_1_1_1_1);
+        
+        JButton button2_1_1_1_1_1_1_1_1 = new JButton(" ");
+        button2_1_1_1_1_1_1_1_1.setBounds(6, 683, 178, 34);
+        sidePanel.add(button2_1_1_1_1_1_1_1_1);
         getContentPane().add(contentPanel, BorderLayout.CENTER);
 
         setLocationRelativeTo(null);
